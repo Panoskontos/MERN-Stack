76063c4e261ae276ec26a24cebc82e31b7eceff4
Panagiotis Kontos <panagiotiskontos@adminisatorsMBP.station>
2022-08-02 18:54:00 +0300

    first commit

--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -1,9 +1,24 @@
 const router = require('express').Router()
 let Exercise = require('../models/exercise')
 
-router.route("/").get((req,res)=>{
-   res.send("test")
+router.route("/").get(async (req,res)=>{
+    try{
+        const users = await Exercise.find()
+        res.json(users)
+
+    }catch(err){
+        res.status(400).json('error'+err)}
 })
+
+router.route("/").post((req,res)=>{
+    const newExercise = new Exercise({
+        username: req.body.username,
+        description: req.body.description,
+        duration: req.body.duration
+    })
+    newExercise.save().then(()=>res.json(newExercise)).catch(err=>res.status(400).json("Error"+err))
+})
+
 
 
 module.exports = router
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,20 @@
 const router = require('express').Router()
 let User = require('../models/user')
 
-router.route("/").get((req,res)=>{
-    User.find().then(users=>res.json(users))
-    .carch(err=>res.status(400).json('error'+err))
+router.route("").get(async (req,res)=>{
+    try{
+        const users = await User.find()
+        res.json(users)
+
+    }catch(err){
+        res.status(400).json('error'+err)}
+})
+
+router.route("/add").post((req,res)=>{
+    const newUser = new User({
+        username:req.body.username,
+    })
+    newUser.save().then(()=>res.json(newUser)).catch(err=>res.status(400).json("Error"+err))
 })
 
 
