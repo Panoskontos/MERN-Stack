f95edc37e3644816842c207e4de1558120dd7151
Panagiotis Kontos <panagiotiskontos@adminisatorsMBP.station>
2022-08-02 17:20:18 +0300

    first commit

---
+++ backend/index.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+require('dotenv').config()
+
+const app = express();
+const port = process.env.PORT || 7001;
+
+// middle ware
+app.use(cors())
+app.use(express.json())
+
+
+// connect Mongo db
+mongoose.connect('mongodb://localhost:27017/merndb',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+);
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", function () {
+  console.log("Connected successfully");
+});
+
+
+
+app.get('/', (req,res) => {
+    res.send("Home")
+})
+
+
+
+app.listen(port, ()=>{
+    console.log("Server is running on port: " +port)
+})
