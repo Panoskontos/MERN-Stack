fe17e9dcd0ac1f23bfa2740ee874f1ca7ee23483
Panagiotis Kontos <panagiotiskontos@DESKTOP-8GVC81T.station>
2022-08-07 17:11:20 +0300

    added api calls

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-// const cors = require('cors')
+const cors = require('cors')
 const mongoose = require('mongoose')
 
 require('dotenv').config()
@@ -8,7 +8,7 @@ const app = express();
 const port = process.env.PORT || 7001;
 
 // middle ware
-// app.use(cors())
+app.use(cors())
 app.use(express.json())
 
 
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,66 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import MyAppBar from './components/MyAppBar';
+import BasicCard from './components/BasicCard';
+import { Grid } from '@mui/material';
+import { useState,useEffect } from 'react';
 
 
 function App() {
-  return (
-    <div className="App">
-      God damn
-    </div>
-  );
+
+  const [exercises, setExercises] = useState([]);
+
+
+
+  async function get_exercises(){
+    var requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+    fetch("http://localhost:7001/exercises", requestOptions)
+      .then(response => response.json())
+      .then(result => setExercises(result))
+      .catch(error => console.log('error', error));
+    }
+
+
+    useEffect(() => {
+      get_exercises()
+    },[]);
+
+  
+
+
+    
+  return <>
+       { console.log(exercises)}
+        <MyAppBar />
+        <br/>
+
+
+
+        <Grid
+          container
+          direction="row"
+          justifyContent="center"
+          alignItems="center"
+        >
+               
+               
+               {exercises.map((i)=>(
+                 <BasicCard duration={i.duration} description={i.description} id={i._id} />
+               ))}
+                
+            
+
+        </Grid>
+
+
+
+  </>
+  
 }
+
+
 
 export default App;
