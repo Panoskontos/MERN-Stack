8612d1ba8d9d5cede1eccb27d813424ca5aaea2e
Panagiotis Kontos <panagiotiskontos@DESKTOP-8GVC81T.station>
2022-08-07 16:12:27 +0300

    updated

--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -20,6 +20,34 @@ router.route("/").post((req,res)=>{
 })
 
 
+router.route("/:id").get((req,res)=>{
+    Exercise.findById(req.params.id)
+    .then(i=>res.json(i))
+    .catch(err=>res.status(400).json('error'+err))
+})
+
+
+router.route("/:id").delete((req,res)=>{
+    Exercise.findByIdAndDelete(req.params.id)
+    .then(()=>res.json("Item was deleted"))
+    .catch(err=>res.status(400).json('error'+err))
+})
+
+
+// PUT
+router.route("/:id").put((req,res)=>{
+    Exercise.findById(req.params.id)
+    .then(i=>{
+        i.username = req.body.username
+        i.description = req.body.description
+        i.duration = Number(req.body.duration)
+
+        i.save()
+        .then(()=>res.json('exercise updated'))
+        .catch(err=>res.status(400).json('error:'+err))
+    })
+})
+
 
 module.exports = router
 
